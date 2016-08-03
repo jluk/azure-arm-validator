b17b1b70f28700766162130319afe265bf050fcc
jluk <juluk@microsoft.com>
2016-08-03 13:35:59 -0700

    updated to neil's branch

---
+++ replacementSchemes/genSshPubKey.js
@@ -0,0 +1,6 @@
+var module = require('module')
+var conf = require('../modules/config');
+
+var replacementScheme = {'indicator': 'SSH_KEY_REPLACE_INDICATOR', 'value': conf.get('SSH_PUBLIC_KEY')};
+
+module.exports = {'replacementScheme': replacementScheme}
