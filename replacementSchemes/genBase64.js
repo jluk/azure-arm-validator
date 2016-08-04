c1e5de13de42ea1e5e1b36aefa6421d4b71dd193
jluk <juluk@microsoft.com>
2016-08-04 15:53:33 -0700

    more replacementschemes

--- replacementSchemes/genBase64.js
+++ replacementSchemes/genBase64.js
@@ -3,6 +3,6 @@ var Guid = require('guid');
 
 var conf = require('../modules/config');
 
-var replacementScheme = {'indicator': 'BASE64_REPLACE_INDICATOR', 'value': Guid.raw().subString(0,5).toString('base64')};
+var replacementScheme = {'indicator': 'BASE64_REPLACE_INDICATOR', 'value': 'test'};
 
 module.exports = {'replacementScheme': replacementScheme}
